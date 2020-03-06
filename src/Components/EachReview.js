ec7b9224f27db0c592aa0e7e6f7b6d230b8a5fc0
Sui.Young <suisauce.codes@gmail.com>
2020-03-06 10:00:18 -0500

    adding EachReview and ReviewPanel

---
+++ src/Components/EachReview.js
@@ -0,0 +1,19 @@
+// a couple of functions from the React library
+import React from "react";
+
+const EachReview = ({ revProp }) => {
+  // 🧠 function to remove messages
+    // let remove = key => {
+    //     const dbRef = firebase.database().ref();
+    //     dbRef.child(msgProp.key).remove();
+    // };
+
+    console.log(revProp)
+    return (
+        <div key={revProp.key}>
+
+        </div>
+    );
+};
+
+export default EachReview;
