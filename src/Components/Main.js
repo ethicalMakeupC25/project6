f360fa067b3701b30ff85368e8176da6239c1074
Naveen2905 <naveenmalhotra05@gmail.com>
2020-03-08 17:28:32 -0400

    modified

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,6 +3,7 @@ import Search from './Search';
 import Results from './Results';
 import ResultDetails from './ResultDetails'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import FilterResults from './FilterResults';
 
 class Main extends Component {
     constructor() {
@@ -11,8 +12,8 @@ class Main extends Component {
         this.state = {
             isSearched: false,
             searchInput: '',
-
-            filteredResults: []
+            filteredResults: [],
+            refinedItems: [],
         }
     }
 
@@ -31,16 +32,33 @@ class Main extends Component {
         const filteredArray = this.props.veganProducts.filter(product => {
             return product.product_type === this.state.searchInput
         });
-
+        // console.log(filteredArray);
         this.setState({
             filteredResults: filteredArray
         });
+    }
+
+
+    newResults = () => {
+        this.state.filteredResults.filter(eachItem => {
+            // console.log(eachItem.tag_list) 
+            console.log (eachItem.tag_list.includes(this.state.refinedItems.toString()))
+        });
+        console.log(this.state.refinedItems.toString());
+        
+    }
+
+    updateItems = (e, refinedCategory) => {
+        this.setState({
+            refinedItems: refinedCategory,
+        }, this.newResults)
     }
 
     render() {
         return (
             <main>
                 <Search veganProducts={this.props.veganProducts} handleSearchInput={this.handleSearchInput} />
+                <FilterResults updaterefinedItems={this.updateItems} />
                 <Route path="/products/:productID" render={() => <ResultDetails filteredResults={this.props.filteredResults} />} />
                 {this.state.isSearched ? <Results filteredResults={this.state.filteredResults} /> : null}
             </main>
