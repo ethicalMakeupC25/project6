8c647227dd5968b11bba9a62d9182c154e9201b9
Stephanie Kerr <steph.vm.kerr@gmail.com>
2020-03-09 17:41:55 -0400

    Updates

---
+++ precache-manifest.27a4b64d06d79f536826bbfc0945f693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5d0be473c95d9aa89d99ed8b3d97c06",
+    "url": "/project6/index.html"
+  },
+  {
+    "revision": "ee917e8c2c8285432aed",
+    "url": "/project6/static/css/main.ee421917.chunk.css"
+  },
+  {
+    "revision": "cd08addef00742f86376",
+    "url": "/project6/static/js/2.d304fd1c.chunk.js"
+  },
+  {
+    "revision": "7ba5a91c6aa4ae9cc068a25fb13d169c",
+    "url": "/project6/static/js/2.d304fd1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee917e8c2c8285432aed",
+    "url": "/project6/static/js/main.084b0351.chunk.js"
+  },
+  {
+    "revision": "a48f442834976427eff9",
+    "url": "/project6/static/js/runtime-main.742acfe4.js"
+  }
+]);
