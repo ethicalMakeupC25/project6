2345deba13ebed962c63b05b8f95a208f5489e08
Sui.Young <suisauce.codes@gmail.com>
2020-04-03 15:24:18 -0400

    remove custom cursor

---
+++ src/Components/ResultDetails.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class ResultDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // this.props
+  }
+
+  render() {
+    return(
+      <div className="productDetails">
+        <div className="productImage">
+          <img src="" alt=""/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ResultDetails;
